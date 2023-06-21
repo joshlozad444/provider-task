9fac2e246cdeace0cc474740bea4148a3a6bc345
unknown <johnjoshualozada@gmail.com>
2023-06-21 21:09:12 +0800

    initial commit

---
+++ app/Http/Controllers/ProviderController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProviderController extends Controller
+{
+    public function getProviders()
+    {
+
+    }
+
+    public function updateProvider()
+    {
+
+    }
+
+    public function deleteProvider()
+    {
+        
+    }
+}
---
+++ resources/views/app.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ShipERP PHP Technical Exam</title>
+</head>
+<body>
+    <div class="provider-add">
+        <input type="text" name="provider-name" placeholder="Name of data provider">
+        <input type="text" name="provider-url" placeholder="URL of data provider">
+        <button>Add</button>
+    </div>
+    <div class="provider-entries">
+        <div></div>
+        <table>
+            <thead>
+                <tr>
+                    <td>Name</td>
+                    <td>URL</td>
+                    <td>Actions</td>
+                </tr>
+            </thead>
+                <tr>
+                    <td>Sample name</td>
+                    <td>Sample URL</td>
+                    <td>
+                        <button>Try me!</button>
+                        <button>Edit</button>
+                        <button>Delete</button>
+                    </td>
+                </tr>
+        </table>
+    </div>
+    <div class="image-container">
+        <img src="" alt="">
+    </div>
+</body>
+</html>
